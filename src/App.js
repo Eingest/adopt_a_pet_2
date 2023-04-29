a062d2a56fcc49fac689b30beb3481383d5b1ce2
Eingest <ak0805@live.de>
2023-04-29 16:06:05 +0200

    adding a search feature

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import {
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
-      <Route path=":type/:id" element={<PetDetailsPage/>} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path=":type/:id" element={<PetDetailsPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path=":type" element={<HomePage />} />
     </Route>
